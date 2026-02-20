f7b88692001aefbed7200d563059dcefa9476861
Abdula Muhammadov <150147805+labubuahhginger@users.noreply.github.com>
2026-02-20 09:57:04 +0300

    Update display_wrapper.h

--- include/display_wrapper.h
+++ include/display_wrapper.h
@@ -72,20 +72,23 @@
   };
   
 #else
-  #include <Adafruit_GFX.h>
-  #include <Adafruit_SSD1306.h>
-  
-  class DisplayWrapper : public Adafruit_SSD1306 {
-  public:
-    DisplayWrapper() 
-      : Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1) {}
-    
-    void begin() {
-      Adafruit_SSD1306::begin(SSD1306_SWITCHCAPVCC, 0x3C);
-      setTextColor(SSD1306_WHITE);   // use explicit macro
-      setTextSize(1);
-      clearDisplay();
-    }
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+class DisplayWrapper : public Adafruit_SSD1306 {
+public:
+  static const uint16_t COLOR_WHITE = SSD1306_WHITE;
+  static const uint16_t COLOR_BLACK = SSD1306_BLACK;
+
+  DisplayWrapper()
+    : Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1) {}
+
+  void begin() {
+    Adafruit_SSD1306::begin(SSD1306_SWITCHCAPVCC, 0x3C);
+    setTextColor(SSD1306_WHITE);
+    setTextSize(1);
+    clearDisplay();
+  }
     
     void printf(const char* fmt, ...) {
       char buffer[128];
